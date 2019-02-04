99271bb02b90448ea6920dd7d32c6c98c239b577
payamdaliri <payamdaliri96@gmail.com>
2019-02-04 20:01:54 +0330

    df

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,24 +23,62 @@ $(document).ready(function () {
 
 
 
-    // dataUrl = "https://borsam.github.io/assets/jsons/content-test.json"
 
 
-    var data;
-    $.ajax({
-        dataType: "json",
-        url: 'https://borsam.github.io/assets/jsons/content-test.json',
-        data: data,
-        success: function (data) {
-            // begin accessing JSON data here
-            console.log(data[0].service);
-        }
-    });
+    // var data;
+    // $.ajax({
+    //     dataType: "json",
+    //     url: 'https://borsam.github.io/assets/jsons/content-test.json',
+    //     data: data,
+    //     success: function (data) {
+    //         // begin accessing JSON data here
+            
+    //         for (var i = 0; i < data.length; i++) {
+    //             $('.images_thumbnails').append('<div class="col-lg-4 col-sm-6  mb-4">' + '<div class="w-100">' + '<img class="service_img" src="' + data[i].properties[i].pics[i].img + '" alt="Card image cap">' + '</div>' + '</div>');
 
-    // $.getJSON('https://borsam.github.io/assets/jsons/content-test.json', function (data) {
-	// 	// begin accessing JSON data here
-	// 		console.log(data[0].service);
-	// });
+
+
+    //         }
+    //         console.log(data[0].service);
+    //     }
+    // });
+
+
+
+
+    // owl.trigger('add.owl.carousel', [jQuery('<div class="item" data-hash="' + contents[3].properties[0].pics[post].id + '">' + '<img src="' + contents[3].properties[0].pics[post].img + '" alt="">' + '</div>')]);
+
+    // <div class="col-lg-4 col-sm-6  mb-4 ">
+    //     <div class="w-100">
+    //         <img data-toggle="modal" data-target="#exampleModal" class=" service_img" src="https://picsum.photos/600/400"
+    //             alt="Card image cap">
+    //     </div>
+    // </div>
+    // <div class="col-lg-4 col-sm-6 mb-4">
+    //     <div class="w-100">
+    //         <img class=" service_img" src="https://picsum.photos/600/400" alt="Card image cap">
+    //     </div>
+    // </div>
+    // <div class="col-lg-4 col-sm-6 mb-4">
+    //     <div class=" w-100">
+    //         <img class=" service_img" src="https://picsum.photos/600/400" alt="Card image cap">
+    //     </div>
+    // </div>
+    // <div class="col-lg-4 col-sm-6 mb-4">
+    //     <div class=" w-100">
+    //         <img class=" service_img" src="https://picsum.photos/600/400" alt="Card image cap">
+    //     </div>
+    // </div>
+    // <div class="col-lg-4 col-sm-6 mb-4">
+    //     <div class=" w-100">
+    //         <img class=" service_img" src="https://picsum.photos/600/400" alt="Card image cap">
+    //     </div>
+    // </div>
+    // <div class="col-lg-4 col-sm-6 mb-4">
+    //     <div class=" w-100">
+    //         <img class=" service_img" src="https://picsum.photos/600/400" alt="Card image cap">
+    //     </div>
+    // </div>
 
 
     //Responsive Functions
