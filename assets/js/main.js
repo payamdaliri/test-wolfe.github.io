2af81aadc8de630ab23866a1772f29267d0d5352
payamdaliri <payamdaliri96@gmail.com>
2019-02-04 20:25:43 +0330

    ss

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,53 @@ $(document).ready(function () {
     });
 
 
+    // Contact Form Codes
+    // Get the form.
+    var form = $('#ajax-contact');
 
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    // TODO: The rest of the code will go here...
+    $(form).submit(function (event) {
+        // Stop the browser from submitting the form.
+        event.preventDefault();
+
+        // TODO
+    });
+
+    // Serialize the form data.
+    var formData = $(form).serialize();
+
+    // Submit the form using AJAX.
+    $.ajax({
+        type: 'POST',
+        url: $(form).attr('action'),
+        data: formData
+    }).done(function (response) {
+        // Make sure that the formMessages div has the 'success' class.
+        $(formMessages).removeClass('error');
+        $(formMessages).addClass('success');
+
+        // Set the message text.
+        $(formMessages).text(response);
+
+        // Clear the form.
+        $('#name').val('');
+        $('#email').val('');
+        $('#message').val('');
+    }).fail(function (data) {
+        // Make sure that the formMessages div has the 'error' class.
+        $(formMessages).removeClass('success');
+        $(formMessages).addClass('error');
+
+        // Set the message text.
+        if (data.responseText !== '') {
+            $(formMessages).text(data.responseText);
+        } else {
+            $(formMessages).text('Oops! An error occured and your message could not be sent.');
+        }
+    });
 
 
     // var data;
@@ -32,7 +78,7 @@ $(document).ready(function () {
     //     data: data,
     //     success: function (data) {
     //         // begin accessing JSON data here
-            
+
     //         for (var i = 0; i < data.length; i++) {
     //             $('.images_thumbnails').append('<div class="col-lg-4 col-sm-6  mb-4">' + '<div class="w-100">' + '<img class="service_img" src="' + data[i].properties[i].pics[i].img + '" alt="Card image cap">' + '</div>' + '</div>');
 
