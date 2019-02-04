5715b983949a20269c56a0270e08d65dcc992232
payamdaliri <payamdaliri96@gmail.com>
2019-02-04 18:51:28 +0330

    asfa

--- mail/mail.php
+++ mail/mail.php
@@ -1,15 +1,10 @@
-<?php
-
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $message = $_POST['message'];
-
-    $mailTo = "info@payamdaliri.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
-
-    mail($mailTo, $txt, $headers);
-    header("Location: index.php?mailsend");
-}
->
+<?php $name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$formcontent="From: $name \n Message: $message";
+$recipient = "emailaddress@here.com";
+$subject = "Contact Form";
+$mailheader = "From: $email \r\n";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+echo "Thank You!";
+?>
