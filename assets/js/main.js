33f1ab1d5ddfc7aef551877bf3964cc7b90d28c9
payamdaliri <payamdaliri96@gmail.com>
2019-02-04 20:30:25 +0330

    ds

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,17 +57,6 @@ $(document).ready(function () {
         $('#name').val('');
         $('#email').val('');
         $('#message').val('');
-    }).fail(function (data) {
-        // Make sure that the formMessages div has the 'error' class.
-        $(formMessages).removeClass('success');
-        $(formMessages).addClass('error');
-
-        // Set the message text.
-        if (data.responseText !== '') {
-            $(formMessages).text(data.responseText);
-        } else {
-            $(formMessages).text('Oops! An error occured and your message could not be sent.');
-        }
     });
 
 
