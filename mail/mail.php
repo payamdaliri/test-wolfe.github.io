36015988c8e34bc26141e81246e9cde17f58cecb
payamdaliri <payamdaliri96@gmail.com>
2019-02-04 18:22:20 +0330

    SS

--- mail/mail.php
+++ mail/mail.php
@@ -2,7 +2,7 @@
 $email = $_POST['email'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message";
-$recipient = "payamdaliri96@gmail.com";
+$recipient = "payamdaliri96@mail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
