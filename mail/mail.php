805cc55513030a522ba35504f5ddf5dadb6723ba
payamdaliri <payamdaliri96@gmail.com>
2019-02-04 18:31:33 +0330

    s

--- mail/mail.php
+++ mail/mail.php
@@ -1,10 +1,15 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "payamdaliri96@mail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
-?>
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $message = $_POST['message'];
+
+    $mailTo = "payamdaliri96@mail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+    mail($mailTo, $txt, $headers);
+    header("Location: index.php?mailsend");
+}
+>
